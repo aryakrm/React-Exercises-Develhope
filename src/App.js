46a463e6298e7c87bfbf725e9cc195b2d1886045
Arya <arya@mahsas-mbp.local>
2022-11-04 16:32:12 +0300

    comp2 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Hello from "./Hello";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Hello extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Hello;
