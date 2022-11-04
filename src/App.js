6e1c8ff73f2f7e89f8ed7d95572a762ebd109e7e
Arya <arya@mahsas-mbp.local>
2022-11-04 16:22:43 +0300

    comp1 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 
 function App() {
-  const sum = (a, b) => <h2>{a + b}</h2>;
-  return <div className="App">{sum(2, 5)}</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
