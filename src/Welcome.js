7677c1ab5648c8a5cbb4abc5598e5610de8407d4
Arya <arya@mahsas-mbp.local>
2022-11-04 22:08:04 +0300

    cr5 done

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,8 +3,8 @@ import Age from "./Age";
 function Welcome({ name, age }) {
   return (
     <div>
-      {name === "John" && age > 18 && age < 65 && <p>Welcome {name}!</p>}
-      {age > 18 && age < 65 && <Age age={age} />}
+      {name === "John" && <p>Welcome {name}!</p>}
+      {age > 18 && age < 65 ? <Age age={age} /> : <p>You are very young!</p>}
     </div>
   );
 }
