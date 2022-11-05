7c00f9e1dee48612f51485a00e6c23af7171a86b
Arya <arya@mahsas-mbp.local>
2022-11-05 22:46:56 +0300

    form5 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./Login";
+import UncontrolledLogin from "./UncontrolledLogin";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <UncontrolledLogin />
     </div>
   );
 }
---
+++ src/UncontrolledLogin.js
@@ -0,0 +1,76 @@
+import React, { createRef } from "react";
+
+class UncontrolledLogin extends React.Component {
+  _refForm = createRef();
+  handleEvent = (event) => {
+    event.preventDefault();
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+    const remember = event.target.elements.remember.checked;
+
+    console.log({
+      username,
+      password,
+      remember,
+    });
+  };
+
+  handlePrefill = () => {
+    this._refForm.current.elements.username.value = "Arya";
+    this._refForm.current.elements.password.value = "123456";
+    this._refForm.current.elements.remember.checked = true;
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginTop: "20px",
+          marginLeft: "40rem",
+        }}
+      >
+        <form ref={this._refForm} onSubmit={this.handleEvent}>
+          <input
+            placeholder="Name"
+            name="username"
+            type="text"
+            style={{ marginTop: "1rem" }}
+          />
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            style={{ marginTop: "1rem" }}
+          />
+          <div>
+            <label htmlFor="remember" style={{ fontSize: "14px" }}>
+              Remember me
+            </label>
+            <input
+              name="remember"
+              type="checkbox"
+              style={{ marginTop: "1rem" }}
+            />
+          </div>
+          <button type="reset">reset</button>
+          <button
+            type="submit"
+            style={{
+              marginTop: "1rem",
+            }}
+          >
+            log in
+          </button>
+          <button type="button" onClick={this.handlePrefill}>
+            Prefill
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UncontrolledLogin;
