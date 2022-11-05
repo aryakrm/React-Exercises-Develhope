12072981ba69c3dcd4c0957bda79b5b01c3f031f
Arya <arya@mahsas-mbp.local>
2022-11-05 19:40:11 +0300

    forms2 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import InteractiveWelcome from "./InteractiveWelcome";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <InteractiveWelcome />
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+  };
+  handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    const type = event.target.type;
+    const checked = event.target.checked;
+
+    this.setState({
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginTop: "20px",
+          marginLeft: "40rem",
+        }}
+      >
+        <input
+          placeholder="Name"
+          name="username"
+          type="text"
+          value={this.state.username}
+          style={{ marginTop: "1rem" }}
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="Password"
+          name="password"
+          type="password"
+          value={this.state.password}
+          style={{ marginTop: "1rem" }}
+          onChange={this.handleChange}
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          style={{ marginTop: "1rem" }}
+          checked={this.state.remember}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default Login;
