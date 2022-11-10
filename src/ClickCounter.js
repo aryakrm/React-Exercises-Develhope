aa792064b958df18ec6e1c501bd64de1fa499333
Arya <arya@mahsas-mbp.local>
2022-11-10 15:28:37 +0300

    useEffect1 done

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function ClickCounter() {
   let [count, setCount] = useState(0);
   const clickHandler = (event) => {
     setCount(count + 1);
   };
+  useEffect(() => {
+    console.log(`the number is ${count}`);
+    return () => {
+      console.log(`the number was ${count}`);
+    };
+  }, [count]);
   return (
     <div>
       <h1>{count}</h1>
