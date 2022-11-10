d14976621db8686d9712246110626b211f215914
Arya <arya@mahsas-mbp.local>
2022-11-10 17:40:33 +0300

    useEffect1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import ClickCounter from "./ClickCounter";
 
 export default function App() {
+  let [count, setCount] = useState(0);
+
+  const onCounterChange = (event) => {
+    setCount(count + 1);
+  };
+  useEffect(() => {
+    console.log(`the number is ${count}`);
+    return () => {
+      console.log(`the number was ${count}`);
+    };
+  }, [count]);
+
   return (
     <div className="App">
-      <ClickCounter />
+      <ClickCounter click={onCounterChange} count={count} />
     </div>
   );
 }
--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,20 +1,8 @@
-import { useState, useEffect } from "react";
-
-export default function ClickCounter() {
-  let [count, setCount] = useState(0);
-  const clickHandler = (event) => {
-    setCount(count + 1);
-  };
-  useEffect(() => {
-    console.log(`the number is ${count}`);
-    return () => {
-      console.log(`the number was ${count}`);
-    };
-  }, [count]);
+export default function ClickCounter({ click, count }) {
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={clickHandler}>increase +</button>
+      <button onClick={click}>increase +</button>
     </div>
   );
 }
