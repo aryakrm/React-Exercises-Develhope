b4bec86b1718095115b75d8df04bb5d5138b4084
Arya <arya@mahsas-mbp.local>
2022-11-13 15:37:37 +0300

    router2 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import ClickCounter from "./ClickCounter";
 
 export default function App() {
   return (
     <div className="App">
-      <ClickCounter />
+      <Link to="/counter">Go to Counter</Link>
+      <Routes>
+        <Route path="/counter" element={<ClickCounter />} />
+      </Routes>
     </div>
   );
 }
