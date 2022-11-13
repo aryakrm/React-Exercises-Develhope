402b2a1605c1ff7107237166bde02463aa80391e
Arya <arya@mahsas-mbp.local>
2022-11-13 15:24:36 +0300

    router1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Welcome from "./Welcome";
 
 function App() {
   return (
     <div className="App">
-      <Welcome name="Arya" />
+      <Routes>
+        <Route path="/" element={<Welcome name="Arya" />} />
+      </Routes>
     </div>
   );
 }
